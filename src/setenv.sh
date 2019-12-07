0891d3b7002fe0b0e6573ec406772da1f723ca8b
gaosong666 <45898222+gaosong666@users.noreply.github.com>
2019-12-07 13:51:55 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,8 +1,8 @@
 export GITACCOUNT=gaosong
 
 export MYCLUSTER=tektonknativebeijing27
-export KUBECONFIG=
+export KUBECONFIG=/tmp/ic/AB/.bluemix/plugins/container-service/clusters/tektonknativebeijing27/kube-config-tok04-tektonknativebeijing27.yml
 
-export NAMESPACE=
+export NAMESPACE=tektonknativebeijing27
 export EMAIL=1005678398@qq.com
-export INGRESS=
+export INGRESS=tektonknativebeijing27-0e3e0ef4c9c6d831e8aa6fe01f33bfc4-0001.jp-tok.containers.appdomain.cloud
